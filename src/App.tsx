034cbec5cc7bb5d8bbc6ee94cc89902e4e76d74a
Peter Lewandowski <mapiotr2010@gmail.com>
2023-04-24 16:28:45 -0400

    restructure main.tsx and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import './App.css'
+import { createBrowserRouter, Routes, Route  } from "react-router-dom";
+import { Layout } from "antd";
+import "./App.css";
+
+import { UserContextProvider } from "./context/UserContext";
+
+const { Header, Content, Footer } = Layout;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/create-strategy",
+    element: <CreateStrategy />,
+  },
+  {path:'/login', element: {<AnonRoute>}}
+]);
 
 function App() {
-
-
   return (
-    <>
-      <h1>My App</h1>
-    </>
-  )
+    <Layout>
+      <Header>Header</Header>
+      <Content>
+        <Routes>
+
+            <Route path="/" element={<Home />} />
+
+        </Routes>
+      </Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import "./index.css";
-import { UserContextProvider } from "./context/UserContext";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <RouterProvider router={router} />
-    </UserContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
