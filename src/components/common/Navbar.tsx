913ac754e454f0030d73b49d84e7f1da0ab3744a
Peter Lewandowski <mapiotr2010@gmail.com>
2023-05-21 19:35:34 -0400

    refactor Navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from "react-router";
 import { UserContext } from "../../context/UserContext";
-import { Button } from "antd";
+import { Button, Col, Row, Space } from "antd";
 import LogoImage from "/easyfi-logo.png";
 
 export default function Navbar() {
@@ -18,44 +18,32 @@ export default function Navbar() {
     signOut(auth).then(() => navigate("/"));
   };
 
-  const navButtons = (condition: any) => {
-    if (condition) {
-      return (
-        <span>
-          <Button onClick={() => navigate("/dashboard")}>Dashboard</Button>
-          <Button onClick={handleLogout}>Sign Out</Button>
-        </span>
-      );
-    } else {
-      return (
-        <span>
-          <Button type="primary" onClick={handleClickLogin}>
-            Sign in!
-          </Button>
-        </span>
-      );
-    }
-  };
+  const navButtons = user ? (
+    <>
+      <Button onClick={() => navigate("/dashboard")}>Dashboard</Button>
+      <Button onClick={handleLogout}>Sign Out</Button>
+    </>
+  ) : (
+    <>
+      <Button type="primary" onClick={handleClickLogin}>
+        Sign in!
+      </Button>
+    </>
+  );
 
   return (
-    <nav
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-      }}
-    >
-      <div>
-        <img
-          style={{
-            width: "25%",
-          }}
-          src={LogoImage}
-          onClick={() => navigate("/")}
-          alt="easyFi: Investment Planning. Made Easy."
-        />
-      </div>
-      <div>{navButtons(user)}</div>
-    </nav>
+    <Row justify="space-between">
+      <Col>
+        <Row>
+          <img
+            style={{ width: "30%", height: "auto" }}
+            src={LogoImage}
+            onClick={() => navigate("/")}
+            alt="easyFi: Investment Planning. Made Easy."
+          />
+        </Row>
+      </Col>
+      <Space>{navButtons}</Space>
+    </Row>
   );
 }
